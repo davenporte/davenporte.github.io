df93444147c84126e44b28acedb172a1cbfc4b45
davenporte <davenporte@users.noreply.github.com>
2020-05-03 12:24:07 -0400

    margin tweaks

---
+++ hw/final-project/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+      // all js goes in here
+      console.log("we loaded");
+
+      $("#nav1")
+        .mouseover(function() {
+          $("#nav1").css("color", "#F9374D");
+        })
+      // why isn't this working

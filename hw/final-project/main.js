c501dcc5b193f9474cefc0da70df16928236f4e8
davenporte <davenporte@users.noreply.github.com>
2020-05-05 01:26:37 -0400

    started mobile css

--- hw/final-project/main.js
+++ hw/final-project/main.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
           $("#nav1").css("color", "#F9374D");
         })
       // why isn't this working
+
+      // $(".text-about").mouseover(function() {
+      //   $("#learnmore").fadeIn();
+      // })
+      // why is this not working either??
+
+      $("#distraction")
+        .mouseover(function() {
+          $("#distraction").css("color", "#F9374D");
+        })

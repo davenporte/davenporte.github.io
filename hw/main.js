ad6ef686586f3dcc6efdb491242e3323c0a3f4cc
davenporte <davenporte@users.noreply.github.com>
2020-05-06 19:41:46 -0400

    title spacing, alignment

---
+++ hw/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+      // all js goes in here
+      console.log("we loaded");
+
+      $("#nav1")
+        .mouseover(function() {
+          $("#nav1").css("color", "#F9374D");
+        })
+      // why isn't this working
+
+      // $(".text-about").mouseover(function() {
+      //   $("#learnmore").fadeIn();
+      // })
+      // why is this not working either??
+
+      $("#distraction")
+        .mouseover(function() {
+          $("#distraction").css("color", "#F9374D");
+        })
+      });

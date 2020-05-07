6a8603a8068abd6a11ef9092fc242ca9e62ce213
davenporte <davenporte@users.noreply.github.com>
2020-05-06 21:28:44 -0400

    edited jquery to better fit mobile

--- hw/final-project/main.js
+++ hw/final-project/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // all js goes in here
   console.log("we loaded");
 
+
   $("#nav1")
     .mouseover(function() {
       $("#nav1").css("color", "#F9374D");
@@ -64,90 +65,90 @@ $(document).ready(function() {
     .mouseleave(function() {
       $("#joeholder").css("text-decoration", "none");
     })
-  $("#distraction1").mouseover(function() {
-    $("#cap1").fadeIn();
-  })
+  // $("#distraction1").mouseover(function() {
+  //   $("#cap1").fadeIn();
+  // })
   $("#distraction1").mouseover(function() {
     $("#distraction1").css("max-width", "86%");
   })
   $("#distraction1").mouseleave(function() {
     $("#distraction1").css("max-width", "84%");
   })
-  $("#distraction2").mouseover(function() {
-    $("#cap2").fadeIn();
-  })
+  // $("#distraction2").mouseover(function() {
+  //   $("#cap2").fadeIn();
+  // })
   $("#distraction2").mouseover(function() {
     $("#distraction2").css("max-width", "86%");
   })
   $("#distraction2").mouseleave(function() {
     $("#distraction2").css("max-width", "84%");
   })
-  $("#distraction3").mouseover(function() {
-    $("#cap3").fadeIn();
-  })
+  // $("#distraction3").mouseover(function() {
+  //   $("#cap3").fadeIn();
+  // })
   $("#distraction3").mouseover(function() {
     $("#distraction3").css("max-width", "86%");
   })
   $("#distraction3").mouseleave(function() {
     $("#distraction3").css("max-width", "84%");
   })
-  $("#biostrap1").mouseover(function() {
-    $("#biocap1").fadeIn();
-  })
+  // $("#biostrap1").mouseover(function() {
+  //   $("#biocap1").fadeIn();
+  // })
   $("#biostrap1").mouseover(function() {
     $("#biostrap1").css("max-width", "87%");
   })
   $("#biostrap1").mouseleave(function() {
     $("#biostrap1").css("max-width", "85%");
   })
-  $("#biostrap2").mouseover(function() {
-    $("#biocap2").fadeIn();
-  })
+  // $("#biostrap2").mouseover(function() {
+  //   $("#biocap2").fadeIn();
+  // })
   $("#biostrap2").mouseover(function() {
     $("#biostrap2").css("max-width", "87%");
   })
   $("#biostrap2").mouseleave(function() {
     $("#biostrap2").css("max-width", "85%");
   })
-  $("#off1").mouseover(function() {
-    $("#offcap1").css("color", "black");
-  })
+  // $("#off1").mouseover(function() {
+  //   $("#offcap1").css("color", "black");
+  // })
   $("#off1").mouseover(function() {
     $("#off1").css("max-width", "87%");
   })
   $("#off1").mouseleave(function() {
     $("#off1").css("max-width", "85%");
   })
-  $("#off2").mouseover(function() {
-    $("#offcap2").fadeIn();
-  })
+  // $("#off2").mouseover(function() {
+  //   $("#offcap2").fadeIn();
+  // })
   $("#off2").mouseover(function() {
     $("#off2").css("max-width", "87%");
   })
   $("#off2").mouseleave(function() {
     $("#off2").css("max-width", "85%");
   })
-  $("#off3").mouseover(function() {
-    $("#offcap3").fadeIn();
-  })
+  // $("#off3").mouseover(function() {
+  //   $("#offcap3").fadeIn();
+  // })
   $("#off3").mouseover(function() {
     $("#off3").css("max-width", "87%");
   })
   $("#off3").mouseleave(function() {
     $("#off3").css("max-width", "85%");
   })
-  $("#off4").mouseover(function() {
-    $("#offcap4").fadeIn();
-  })
+  // $("#off4").mouseover(function() {
+  //   $("#offcap4").fadeIn();
+  // })
   $("#off4").mouseover(function() {
     $("#off4").css("max-width", "64%");
   })
   $("#off4").mouseleave(function() {
     $("#off4").css("max-width", "62%");
   })
-  $("#pbg-pic").mouseover(function() {
-    $("#pbg-text").fadeIn();
-  })
+  // $("#pbg-pic").mouseover(function() {
+  //   $("#pbg-text").fadeIn();
+  // })
   $("#pbg-pic").mouseover(function() {
     $("#pbg-pic").css("max-width", "42%");
   })

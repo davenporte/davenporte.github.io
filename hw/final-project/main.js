1a9dce489ac3e97f8f211a3ecbb5ed18667b9fee
davenporte <davenporte@users.noreply.github.com>
2020-05-06 21:06:08 -0400

    jquery interactivity added

--- hw/final-project/main.js
+++ hw/final-project/main.js
@@ -6,15 +6,155 @@ $(document).ready(function() {
     .mouseover(function() {
       $("#nav1").css("color", "#F9374D");
     })
-  // why isn't this working
-
-  // $(".text-about").mouseover(function() {
-  //   $("#learnmore").fadeIn();
-  // })
-  // why is this not working either??
-
-  $("#distraction")
-    .mouseover(function() {
-      $("#distraction").css("color", "#F9374D");
+  $("#nav1")
+    .mouseleave(function() {
+      $("#nav1").css("color", "#797468");
     })
+  $("#nav2")
+    .mouseover(function() {
+      $("#nav2").css("color", "#F9374D");
+    })
+  $("#nav2")
+    .mouseleave(function() {
+      $("#nav2").css("color", "#797468");
+    })
+  $("#nav3")
+    .mouseover(function() {
+      $("#nav3").css("color", "#F9374D");
+    })
+  $("#nav3")
+    .mouseleave(function() {
+      $("#nav3").css("color", "#797468");
+    })
+  $(".dropbtn")
+    .mouseover(function() {
+      $(".dropbtn").css("color", "#F9374D");
+    })
+  $(".dropbtn")
+    .mouseleave(function() {
+      $(".dropbtn").css("color", "#797468");
+    })
+  $("#nav4")
+    .mouseover(function() {
+      $("#nav4").css("color", "#F9374D");
+    })
+  $("#nav4")
+    .mouseleave(function() {
+      $("#nav4").css("color", "#797468");
+    })
+  $("#headerpic").mouseover(function() {
+    $("#headerpic").css("width", "82%");
+  })
+  $("#headerpic").mouseleave(function() {
+    $("#headerpic").css("width", "80%");
+  })
+  $("#joeholder")
+    .mouseover(function() {
+      $("#joeholder").css("color", "#F9374D");
+    })
+  $("#joeholder")
+    .mouseover(function() {
+      $("#joeholder").css("text-decoration", "underline");
+    })
+  $("#joeholder")
+    .mouseleave(function() {
+      $("#joeholder").css("color", "#797468");
+    })
+  $("#joeholder")
+    .mouseleave(function() {
+      $("#joeholder").css("text-decoration", "none");
+    })
+  $("#distraction1").mouseover(function() {
+    $("#cap1").fadeIn();
+  })
+  $("#distraction1").mouseover(function() {
+    $("#distraction1").css("max-width", "86%");
+  })
+  $("#distraction1").mouseleave(function() {
+    $("#distraction1").css("max-width", "84%");
+  })
+  $("#distraction2").mouseover(function() {
+    $("#cap2").fadeIn();
+  })
+  $("#distraction2").mouseover(function() {
+    $("#distraction2").css("max-width", "86%");
+  })
+  $("#distraction2").mouseleave(function() {
+    $("#distraction2").css("max-width", "84%");
+  })
+  $("#distraction3").mouseover(function() {
+    $("#cap3").fadeIn();
+  })
+  $("#distraction3").mouseover(function() {
+    $("#distraction3").css("max-width", "86%");
+  })
+  $("#distraction3").mouseleave(function() {
+    $("#distraction3").css("max-width", "84%");
+  })
+  $("#biostrap1").mouseover(function() {
+    $("#biocap1").fadeIn();
+  })
+  $("#biostrap1").mouseover(function() {
+    $("#biostrap1").css("max-width", "87%");
+  })
+  $("#biostrap1").mouseleave(function() {
+    $("#biostrap1").css("max-width", "85%");
+  })
+  $("#biostrap2").mouseover(function() {
+    $("#biocap2").fadeIn();
+  })
+  $("#biostrap2").mouseover(function() {
+    $("#biostrap2").css("max-width", "87%");
+  })
+  $("#biostrap2").mouseleave(function() {
+    $("#biostrap2").css("max-width", "85%");
+  })
+  $("#off1").mouseover(function() {
+    $("#offcap1").css("color", "black");
+  })
+  $("#off1").mouseover(function() {
+    $("#off1").css("max-width", "87%");
+  })
+  $("#off1").mouseleave(function() {
+    $("#off1").css("max-width", "85%");
+  })
+  $("#off2").mouseover(function() {
+    $("#offcap2").fadeIn();
+  })
+  $("#off2").mouseover(function() {
+    $("#off2").css("max-width", "87%");
+  })
+  $("#off2").mouseleave(function() {
+    $("#off2").css("max-width", "85%");
+  })
+  $("#off3").mouseover(function() {
+    $("#offcap3").fadeIn();
+  })
+  $("#off3").mouseover(function() {
+    $("#off3").css("max-width", "87%");
+  })
+  $("#off3").mouseleave(function() {
+    $("#off3").css("max-width", "85%");
+  })
+  $("#off4").mouseover(function() {
+    $("#offcap4").fadeIn();
+  })
+  $("#off4").mouseover(function() {
+    $("#off4").css("max-width", "64%");
+  })
+  $("#off4").mouseleave(function() {
+    $("#off4").css("max-width", "62%");
+  })
+  $("#pbg-pic").mouseover(function() {
+    $("#pbg-text").fadeIn();
+  })
+  $("#pbg-pic").mouseover(function() {
+    $("#pbg-pic").css("max-width", "42%");
+  })
+  $("#pbg-pic").mouseleave(function() {
+    $("#pbg-pic").css("max-width", "40%");
+  })
+
+
+
 });
